dce9b8d7f9100ba399bf6c209f199952a295dafe
zhaoziwuOfficial <zeewu92@gmail.com>
2026-08-28 22:09:07 +0800

    Delete src-split-1 directory

--- src-split-1/src/constants/languages.js
+++
@@ -1,5 +0,0 @@
-export const LANGS = ['zh-TW', 'en', 'ja', 'ko'];
-
-export const LANG_NAMES = { 'zh-TW': '繁體中文', en: 'English', ja: '日本語', ko: '한국어' };
-
-export const LOCALE_MAP = { 'zh-TW': 'zh-TW', en: 'en-US', ja: 'ja-JP', ko: 'ko-KR' };
